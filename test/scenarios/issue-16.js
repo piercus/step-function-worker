973a2d308b2e5c332031e74d169cfc9a6b5fced0
Pierre <piercus@gmail.com>
2020-06-18 08:37:59 +0200

    random stateMachineName in unt test have now 6 digits

--- test/scenarios/issue-16.js
+++ test/scenarios/issue-16.js
@@ -7,8 +7,8 @@ const cleanUp = require('../utils/clean-up');
 
 const stepFunction = new AWS.StepFunctions();
 const workerName = 'test worker name';
-const stateMachineName = 'test-state-machine-' + Math.floor(Math.random() * 1000);
-const activityName = 'test-step-function-worker-' + Math.floor(Math.random() * 1000);
+const stateMachineName = 'test-state-machine-' + Math.floor(Math.random() * 100000);
+const activityName = 'test-step-function-worker-' + Math.floor(Math.random() * 100000);
 
 process.on('uncaughtException', err => {
 	console.log('uncaughtException', err);
